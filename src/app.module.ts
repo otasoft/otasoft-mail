4a2ade5b9d63c9967d27de22cea4bcc0370f114d
Baroshem <jakub.andrzejewski.dev@gmail.com>
2020-09-12 13:08:57 +0200

    feat: add microservice-connection, db module, and configure docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    DbModule,
+  ],
 })
 export class AppModule {}
