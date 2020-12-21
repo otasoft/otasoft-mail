fd494c1f7aff3975d31056208bb5a28049edaf09
Baroshem <jakub.andrzejewski.dev@gmail.com>
2020-12-21 18:40:53 +0100

    feat: add health check to otasoft-mail

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+
 import { DbModule } from './db/db.module';
+import { HealthModule } from './health/health.module';
 import { SendgridModule } from './sendgrid/sendgrid.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DbModule, SendgridModule],
+  imports: [ConfigModule.forRoot(), DbModule, SendgridModule, HealthModule],
 })
 export class AppModule {}
