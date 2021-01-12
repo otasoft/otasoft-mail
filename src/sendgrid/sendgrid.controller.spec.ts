ef4d9a0a2e5ed414ac6fac4978347d7129960d69
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-12 13:47:04 +0100

    test: fix sendgrid jest tests

--- src/sendgrid/sendgrid.controller.spec.ts
+++ src/sendgrid/sendgrid.controller.spec.ts
@@ -1,12 +1,18 @@
+import { ConfigModule } from '@nestjs/config';
+import { CqrsModule } from '@nestjs/cqrs';
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { SendgridController } from './sendgrid.controller';
+import { SendgridService } from './sendgrid.service';
 
 describe('SendgridController', () => {
   let controller: SendgridController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule, CqrsModule],
       controllers: [SendgridController],
+      providers: [SendgridService],
     }).compile();
 
     controller = module.get<SendgridController>(SendgridController);
